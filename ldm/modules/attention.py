ee454d3672558987212ce2e1ecfa7b80ee4795e9
nev <cy823236858@gmail.com>
2024-03-03 16:50:39 +0800

    initial debugging

--- ldm/modules/attention.py
+++ ldm/modules/attention.py
@@ -339,3 +339,82 @@ class SpatialTransformer(nn.Module):
             x = self.proj_out(x)
         return x + x_in
 
+s=1
+
+class BasicTransformerBlock3D(BasicTransformerBlock):
+
+    def forward(self, x, context=None):
+        return checkpoint(self._forward, (x, context), self.parameters(), self.checkpoint)
+
+    def _forward(self, x, context=None):
+        # num_frames = 4
+        # print('\n\n\n num_frames : ', num_frames , x.shape) #  num_frames :  4 torch.Size([120, 1024, 320])
+        # print('\n context is ', context)
+        x = rearrange(x, "(b f) l c -> b (f l) c", f=4).contiguous()
+        # print('\n x shape after rearrange1 : ', x.shape)
+        x = self.attn1(self.norm1(x), context=context if self.disable_self_attn else None) + x
+        # print('\n x shape after attention1 : ', x.shape )
+        x = rearrange(x, "b (f l) c -> (b f) l c", f=4).contiguous()
+        # print('\n x shape after rearrange2 : ', x.shape , 'context shape is ', context.shape)
+        x = self.attn2(self.norm2(x), context=context) + x
+        # print('\n x shape after attention2 : ', x.shape)
+        x = self.ff(self.norm3(x)) + x
+        return x
+
+
+class SpatialTransformer3D(nn.Module):
+    ''' 3D self-attention '''
+    def __init__(self, in_channels, n_heads, d_head,
+                 depth=1, dropout=0., context_dim=None,
+                 disable_self_attn=False, use_linear=False,
+                 use_checkpoint=True):
+        super().__init__()
+        if exists(context_dim) and not isinstance(context_dim, list):
+            context_dim = [context_dim]
+        self.in_channels = in_channels
+        inner_dim = n_heads * d_head
+        self.norm = Normalize(in_channels)
+        if not use_linear:
+            self.proj_in = nn.Conv2d(in_channels,
+                                     inner_dim,
+                                     kernel_size=1,
+                                     stride=1,
+                                     padding=0)
+        else:
+            self.proj_in = nn.Linear(in_channels, inner_dim)
+
+        self.transformer_blocks = nn.ModuleList(
+            [BasicTransformerBlock3D(inner_dim, n_heads, d_head, dropout=dropout, context_dim=context_dim[d],
+                                   disable_self_attn=disable_self_attn, checkpoint=use_checkpoint)
+                for d in range(depth)]
+        )
+        if not use_linear:
+            self.proj_out = zero_module(nn.Conv2d(inner_dim,
+                                                  in_channels,
+                                                  kernel_size=1,
+                                                  stride=1,
+                                                  padding=0))
+        else:
+            self.proj_out = zero_module(nn.Linear(in_channels, inner_dim))
+        self.use_linear = use_linear
+
+    def forward(self, x, context=None):
+        # note: if no context is given, cross-attention defaults to self-attention
+        if not isinstance(context, list):
+            context = [context]
+        b, c, h, w = x.shape
+        x_in = x
+        x = self.norm(x)
+        if not self.use_linear:
+            x = self.proj_in(x)
+        x = rearrange(x, 'b c h w -> b (h w) c').contiguous()
+        if self.use_linear:
+            x = self.proj_in(x)
+        for i, block in enumerate(self.transformer_blocks):
+            x = block(x, context=context[i])
+        if self.use_linear:
+            x = self.proj_out(x)
+        x = rearrange(x, 'b (h w) c -> b c h w', h=h, w=w).contiguous()
+        if not self.use_linear:
+            x = self.proj_out(x)
+        return x + x_in
